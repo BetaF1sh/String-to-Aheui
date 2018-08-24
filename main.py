b9cf7254108815763b345ab9d4eab2e54285a223
Muhun Kim <iam@muhun.kim>
2018-08-24 23:27:28 +0900

    Less code then before
    Use dict, remove global keyword

--- main.py
+++ main.py
@@ -1,34 +1,10 @@
 inputStr = input("아희로 바꿀 문장을 입력하세요 : ")
 curUNum = 0
-resultStr = ""
-TTh = 0
-Tho = 0
-Hun = 0
-Ten = 0
-One = 0
+resultStr = ''
 
-def firstNum(fnum):
-    global resultStr
-    if fnum == 0:
-        resultStr += ""
-    elif fnum == 1:
-        resultStr += ""
-    elif fnum == 2:
-        resultStr += "박"
-    elif fnum == 3:
-        resultStr += "받"
-    elif fnum == 4:
-        resultStr += "밥"
-    elif fnum == 5:
-        resultStr += "밙"
-    elif fnum == 6:
-        resultStr += "밦"
-    elif fnum == 7:
-        resultStr += "밠"
-    elif fnum == 8:
-        resultStr += "밣"
-    elif fnum == 9:
-        resultStr += "밞"
+first = {2: '박', 3: '받', 4: '밥', 5: '밙', 6: '밦', 7: '밠', 8: '밣', 9: '밞'}
+second = {5: ['밙밚밙밚밥밤', '따따따따따', '따따따따따따'], 4: ['밙밚밙밣', '따따따', '따따따따'], 3: ['밙밚밤', '따따', '따따따'], 2: ['밙반', '따', '따따']}
+third = {0: '', 1: '', 2: '다', 3: '다다', 4: '다다다', 5: '다다다다'}
 
 for curChar in inputStr:
     curUNum = ord(curChar)
@@ -38,66 +14,34 @@ for curChar in inputStr:
     Ten = int((curUNum / 10) % 10)
     One = int(curUNum % 10)
 
-    global digit, plus
     digit = 5
     plus = [0] * 5
     for num in [TTh, Tho, Hun, Ten, One]:
-        firstNum(num)
-        if digit == 5:
-            if num > 0:
-                resultStr += "밙밚밙밚밥밤"
-                if num < 2:
-                    resultStr += "따따따따따"
-                else:
-                    resultStr += "따따따따따따"
-                plus[0] = 1
-        elif digit == 4:
-            if num > 0:
-                resultStr += "밙밚밙밣"
-                if num < 2:
-                    resultStr += "따따따"
-                else:
-                    resultStr += "따따따따"
-                plus[1] = 1
-        elif digit == 3:
-            if num > 0:
-                resultStr += "밙밚밤"
-                if num < 2:
-                    resultStr += "따따"
-                else:
-                    resultStr += "따따따"
-                plus[2] = 1
-        elif digit == 2:
-            if num > 0:
-                resultStr += "밙반"
-                if num < 2:
-                    resultStr += "따"
-                else:
-                    resultStr += "따따"
-                plus[3] = 1
-        elif digit == 1:
+        resultStr = resultStr + first.get(num, '')
+
+        if digit == 1:
             resultStr += ""
             if num > 0:
                 plus[4] = 1
             if num == 1:
-                resultStr += "반밧나"
-        digit -= 1
+                resultStr = resultStr + "반밧나"
+        
+        elif digit > 1 and digit < 6:
+            if num > 0:
+                resultStr = resultStr + second.get(digit)[0]
+                if num < 2:
+                    resultStr = resultStr + second.get(digit)[1]
+                else:
+                    resultStr = resultStr + second.get(digit)[2]
+                plus[5-digit] = 1
+        digit = digit - 1
 
     plusCount = 0
     for i in range(0,5):
-        plusCount += plus[i]
-    if (plusCount == 0) or (plusCount == 1):
-        resultStr += ""
-    elif plusCount == 2:
-        resultStr += "다"
-    elif plusCount == 3:
-        resultStr += "다다"
-    elif plusCount == 4:
-        resultStr += "다다다"
-    elif plusCount == 5:
-        resultStr += "다다다다"
+        plusCount = plusCount + plus[i]
+    resultStr = resultStr + third.get(plusCount)
 
-    resultStr += "맣"
-resultStr += "희"
+    resultStr = resultStr + "맣"
+resultStr = resultStr + "희"
 
 print("아희코드 : " + resultStr)
